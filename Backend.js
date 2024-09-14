de7ffe7685d2b412ad70ace02a94df82bf7448b1
Nish <awesomeny7@gmail.com>
2024-09-14 18:44:18 -0400

    First push

---
+++ Backend.js
@@ -0,0 +1,111 @@
+const firestoreStructure = require('./Firestore_M.json');
+const convert = require('convert-units');
+
+// Sign up for Google Maps API(to factor in transportation costs)
+//console.log(firestoreStructure.GroceryStores.Lidl.Products.product1);
+
+//console.log(convert(100).from('lb').to('kg'));
+
+/* 
+
+    Notes:
+    - Might Need Google Map API for locating stores/finding distance
+    - ...
+
+    Cloud Firestore Structure:
+
+        Collection(GroceryStores) -> Document(Giant, etc.) -> Field
+                                     Document(Giant, etc.) -> Subcollection(Products) -> Document(Proudct1, etc.) -> Field
+                                                                                         Document(Proudct1, etc.) -> Subcollection(Not in use)
+*/
+/*
+
+- 5 Apples
+- 1 Bag of Mixed Nuts
+- 12 Roll of Paper Towels
+- 1 Bag of Carrots
+- 1 Whole Chicken
+- 2 Box of Cheese Crackers
+- 5 Kg of Rice
+
+Set Baseline store as Aldi's
+
+*/
+
+const grocerylist = {
+    1: {
+        name: "Apple",
+        quantity: 5,
+        unit: null,
+    },
+    2: {
+        name: "Mixed nuts",
+        quantity: 1,
+        unit: "Bag"
+    },
+    3: {
+        name: "Paper Towels",
+        quantity: 12,
+        unit: "Roll"
+    },
+    4: {
+        name: "Carrots",
+        quantity: 1,
+        unit: "Bag"
+    },
+    5: {
+        name: "Chicken",
+        quantity: 1,
+        unit: "Whole",
+    },
+    6: {
+        name: "Cheese Crackers",
+        quantity: 2,
+        unit: "Box",
+    },
+    7: {
+        name: "Rice",
+        quantity: 5,
+        unit: "kg",
+    }
+};
+
+
+//Promise???
+
+/*
+1. First access the Grocerylist 
+2. Then access .json Database
+3. Look for each grocery item by name(from grocerylist) in the .json Database
+4. Append Grocery Object to have searched for prices
+*/
+
+
+function getCheapestPrice(object, baseline) {
+    const localobj = object;
+    const maxStores = 5;
+    for (key in grocerylist) {
+        for (x in firestoreStructure.GroceryStores[baseline].Products) {
+            if (localobj[key].name == firestoreStructure.GroceryStores[baseline].Products[x].type) {
+                localobj[key].price = firestoreStructure.GroceryStores[baseline].Products[x].price;
+                localobj[key].groceryStore = baseline;
+            }
+        };
+    };
+    /*
+    - Compare updated localobj prices to another store reference in .json
+    - 
+    */
+   const hitlist = Object.keys(firestoreStructure.GroceryStores);
+   hitlist.splice(hitlist.indexOf('Aldis'), 1);
+   console.log(hitlist)
+   for (y in firestoreStructure.GroceryStores[x].Products) {
+    if (localobj[key].name == firestoreStructure.GroceryStores[baseline].Products[x].type) {
+        if (localobj[key].price > firestoreStructure.GroceryStores[baseline].Products[x].price) {
+            localobj[key].price = firestoreStructure.GroceryStores[baseline].Products[x].price;
+            localobj[key].groceryStore = baseline;
+        }
+    }
+};
+};
+getCheapestPrice(grocerylist, "Aldis");
